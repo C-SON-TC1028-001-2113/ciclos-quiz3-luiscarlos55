afc61d6c52dc4f554abb382af79d8db302089974
luiscarlos55 <a01254044@itesm.mx>
2021-09-03 17:41:28 +0000

    05Promedio Sencillo

--- assignments/05PromedioSencillo/src/exercise.py
+++ assignments/05PromedioSencillo/src/exercise.py
@@ -1,5 +1,17 @@
 def main():
-    #escribe tu código abajo de esta línea
+    n = int(input())
+    k = 0
+    l = 0
+    while k<n:
+        i = float(input())
+        l = l+i
+        k = k+1
+    s=(l/n) 
+    print(s)
+
+
+
+
 
 if __name__=='__main__':
     main()
